ba273b811dcc6d1985d35f85898182118c79ec55
Joel Conalty <joelconalty@gmail.com>
2023-07-04 16:40:41 +0100

    passes all tests on codepen, needs styling

--- src/App.js
+++ src/App.js
@@ -1,60 +1,60 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import './style.scss'
 
-const validKeys = ['q','w','e','a','s','d','z','x','c'];
+const validCodes = [ 81, 87, 69, 65, 83, 68, 90, 88, 67 ]
 
 const soundBank = [
   {
     keyCode: 81,
-    keyTrigger: 'q',
+    keyTrigger: 'Q',
     id: 'Heater-1',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
   },
   {
     keyCode: 87,
-    keyTrigger: 'w',
+    keyTrigger: 'W',
     id: "Heater-2",
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
   },
   {
     keyCode: 69,
-    keyTrigger: 'e',
+    keyTrigger: 'E',
     id: 'Heater-3',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
   },
   {
     keyCode: 65,
-    keyTrigger: 'a',
+    keyTrigger: 'A',
     id: 'Heater-4',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
   },
   {
     keyCode: 83,
-    keyTrigger: 's',
+    keyTrigger: 'S',
     id: 'Clap',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
   },
   {
     keyCode: 68,
-    keyTrigger: 'd',
+    keyTrigger: 'D',
     id: 'Open-HH',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
   },
   {
     keyCode: 90,
-    keyTrigger: 'z',
+    keyTrigger: 'Z',
     id: "Kick-n'-Hat",
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
   },
   {
     keyCode: 88,
-    keyTrigger: 'x',
+    keyTrigger: 'X',
     id: 'Kick',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
   },
   {
     keyCode: 67,
-    keyTrigger: 'c',
+    keyTrigger: 'C',
     id: 'Closed-HH',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
   }
@@ -63,22 +63,17 @@ const soundBank = [
 
 const Drumpad = ({drum, setDrum, volume, setVolume}) => {
 
-  useEffect(() => {
-    return () => {
-      document.addEventListener('keydown', e => {
-      // console.log(e)
-      // console.log(e.keyCode)
-      playSound(e);
-      if(validKeys.includes(e.key)) {
-        setDrum(soundBank.find(x => x.keyCode === e.keyCode).id);
-      }
-    })
+  document.addEventListener('keydown', e => {
+    playSound(e);
+    if(validCodes.includes(e.keyCode)) {
+      setDrum(soundBank.find(x => x.keyCode === e.keyCode).id);
+    }
+  })
     
-  }
-  }, [])
+
 
   const handleClick = (e) => {
-    let sound = document.getElementById(e.target.innerHTML+'-sound');
+    let sound = document.getElementById(e.target.innerHTML[0]);
     console.log(e.target)
     setDrum(soundBank.find(x => x.id.concat('-button') === e.target.id).id);
 
@@ -88,9 +83,10 @@ const Drumpad = ({drum, setDrum, volume, setVolume}) => {
   }
 
   const playSound = async (e) => {
-    if(validKeys.includes(e.key)) {
-      let sound = document.getElementById(e.key+'-sound');
-      await sound.pause();
+    if(validCodes.includes(e.keyCode)) {
+      console.log(e.key)
+      let sound = document.getElementById(e.key.toUpperCase());
+      // await sound.pause();
       sound.currentTime = 0;
       await sound.play();
     }
@@ -101,8 +97,9 @@ const Drumpad = ({drum, setDrum, volume, setVolume}) => {
       {soundBank.map(i => {
         return (
           <>
-            <div onClick={(e)=>handleClick(e)} className="drumpad-button" id={i.id+'-button'}>{i.keyTrigger}</div>
-            <audio className="drumpad-sound" id={i.keyTrigger+'-sound'} src={i.url} volume={volume}></audio>
+            <div onClick={(e)=>handleClick(e)} className="drumpad-button drum-pad" id={i.id+'-button'}>{i.keyTrigger}
+              <audio className="drumpad-sound clip" id={i.keyTrigger} src={i.url} volume={volume} />
+            </div>
           </>
         )
       })}
@@ -122,7 +119,7 @@ const Display = ({drum, volume, setVolume}) => {
 
 
   return (
-    <div className="controls-container">
+    <div className="controls-container" id="display">
       <div className="display-container">
         <p>{drum}</p>
       </div>
@@ -138,7 +135,7 @@ function App() {
   const [volume, setVolume] = useState('1')
   
   return (
-    <div className="wrapper">
+    <div className="wrapper" id="drum-machine">
       <div className="parent-container">
         <Drumpad drum={drum} setDrum={setDrum} volume={volume} setVolume={setVolume}/>
         <Display drum={drum} volume={volume} setVolume={setVolume}/>
@@ -148,3 +145,4 @@ function App() {
 }
 
 export default App;
+
